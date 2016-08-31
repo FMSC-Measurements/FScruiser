2b2c65f989123d430c624db94ecc549691d2285f
Ben Campbell <benjaminjcampbell@fs.fed.us>
2016-08-30 23:54:25 -0600

    got it working!

--- source/FScruiser/FScruiser/ViewModels/MainPageViewModel.cs
+++ source/FScruiser/FScruiser/ViewModels/MainPageViewModel.cs
@@ -1,6 +1,10 @@
 ﻿using Caliburn.Micro;
+using FMSC.ORM.Core.SQL;
+using FMSC.ORM.EntityModel.Attributes;
+using FMSC.ORM.SQLite;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +12,29 @@ using Xamarin.Forms;
 
 namespace FScruiser.ViewModels
 {
+    [EntitySource(SourceName = "Names")]
+    public class Names
+    {
+        [Field(Name = "Name")]
+        public string Name { get; set; }
+    }
+
     public class MainPageViewModel : PropertyChangedBase
     {
         string name;
+
+        public MainPageViewModel()
+        {
+            var ds = new SQLiteDatastore();
+            ds.CreateTable("Names",
+                new ColumnInfo[] { new ColumnInfo("Name", "TEXT", false, false, null) }, false);
+
+            ds.Insert(new Names() { Name = "ben" }, OnConflictOption.Fail);
+
+            var name = ds.From<Names>().Query().FirstOrDefault();
+
+            Name = name?.Name;
+        }
 
         public string Name
         {
