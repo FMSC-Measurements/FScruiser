c24e2d250f74d59bf934e2116b5dbdedf9da72a2
Ben Campbell <benjaminjcampbell@fs.fed.us>
2018-09-12 15:36:51 -0400

    FScruiser.Xamarin.Test initial commit

---
+++ source/FScruiser.Xamarin.Test/Pages/Pages_Common.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using FScruiser.XF.Test;
+using System;
+using System.Linq;
+using System.Reflection;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace FScruiser.XF.Pages
+{
+    public class Pages_Common : TestBase
+    {
+        public Pages_Common(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        /// <summary>
+        /// verify that all page classes initialize and there are no silly mistakes in the xaml
+        /// </summary>
+        [Fact]
+        public void InitializePages()
+        {
+            var assembly = Assembly.GetAssembly(typeof(MainPage));
+
+            var pageTypes = assembly.GetTypes()
+                .Where(t => t.Namespace == "FScruiser.XF.Pages" && t.IsSubclassOf(typeof(Xamarin.Forms.Page)));
+
+            foreach (var t in pageTypes)
+            {
+                Output.WriteLine(t.Name);
+                var page = Activator.CreateInstance(t) as Xamarin.Forms.Page;
+
+                page.Should().NotBeNull();
+            }
+        }
+    }
+}
---
+++ source/FScruiser.Xamarin.Test/TestBase.cs
@@ -0,0 +1,20 @@
+﻿using Xunit.Abstractions;
+
+namespace FScruiser.XF.Test
+{
+    public class TestBase
+    {
+        protected App App { get; }
+        protected ITestOutputHelper Output { get; }
+
+        protected Prism.Ioc.IContainerExtension Container => (Prism.Ioc.IContainerExtension)App.Container;
+
+        public TestBase(ITestOutputHelper output)
+        {
+            Output = output;
+            Xamarin.Forms.Mocks.MockForms.Init();
+
+            App = new App(null);
+        }
+    }
+}
