6387e8cc048d17e76f039ae7edd1abc4ac209d71
Ben Campbell <benjaminjcampbell@fs.fed.us>
2018-10-29 12:30:56 -0400

    update Cruisers page

---
+++ source/FScruiser.Xamarin/ViewModels/ManageCruisersViewModel.cs
@@ -0,0 +1,50 @@
+﻿using FScruiser.XF.Services;
+using Prism.Navigation;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace FScruiser.XF.ViewModels
+{
+    public class ManageCruisersViewModel : ViewModelBase
+    {
+        private Command<string> _addCruiserCommand;
+        private Command<string> _removeCruiserCommand;
+
+        public TallySettingsDataService Data { get; }
+
+        public ICommand AddCruiserCommand => _addCruiserCommand ?? (_addCruiserCommand = new Command<string>(AddCruiser));
+
+        public ICommand RemoveCruiserCommand => _removeCruiserCommand ?? (_removeCruiserCommand = new Command<string>(RemoveCruiser));
+
+        public ManageCruisersViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            Data = new TallySettingsDataService();
+        }
+
+        public void AddCruiser(string cruiser)
+        {
+            Data.AddCruiser(cruiser);
+
+            Data.Save();
+        }
+
+        public void RemoveCruiser(string cruiser)
+        {
+            Data.RemoveCruiser(cruiser);
+
+            Data.Save();
+        }
+
+        protected override void Refresh(INavigationParameters parameters)
+        {
+            Data.Refresh();
+        }
+
+        public override void OnNavigatedFrom(INavigationParameters parameters)
+        {
+            base.OnNavigatedFrom(parameters);
+
+            Data.Save();
+        }
+    }
+}
