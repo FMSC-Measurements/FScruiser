f4fa1ecd3547d06668e7ae434302710902f7e2af
Ben Campbell <benjocamps@gmail.com>
2019-06-26 10:37:53 -0400

    wip filetype associations

--- source/FScruiser.Droid/Activities/MainActivity.cs
+++ source/FScruiser.Droid/Activities/MainActivity.cs
@@ -1,5 +1,6 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using FScruiser.XF;
@@ -9,6 +10,34 @@ using Xamarin.Forms.Platform.Android;
 namespace FScruiser.Droid
 {
     [Activity(Label = "FScruiser", Icon = "@drawable/fscruiser_32dp", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+
+    
+
+    //[IntentFilter(new[] { Intent.ActionView },
+    //    DataScheme = "file",
+    //    DataMimeType = "*/*",
+    //    // for the DataPathPatterns we need to specify many addition patters because of an unfortunate limitation in androids pattern matching. See https://stackoverflow.com/questions/3400072/pathpattern-to-match-file-extension-does-not-work-if-a-period-exists-elsewhere-i/8599921
+    //    DataPathPatterns = new[] { ".*\\\\.cruise", ".*\\\\..*\\\\.cruise", ".*\\\\..*\\\\..*\\\\.cruise", ".*\\\\..*\\\\..*\\\\..*\\\\.cruise" },
+    //    DataHost = "*",
+    //    Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable },
+    //    Icon = "@drawable/fscruiser_32dp")]
+
+    //[IntentFilter(new[] { Intent.ActionView },
+    //    DataScheme = "file",
+    //    // for the DataPathPatterns we need to specify many addition patters because of an unfortunate limitation in androids pattern matching. See https://stackoverflow.com/questions/3400072/pathpattern-to-match-file-extension-does-not-work-if-a-period-exists-elsewhere-i/8599921
+    //    DataPathPatterns = new[] { ".*\\\\.cruise", ".*\\\\..*\\\\.cruise", ".*\\\\..*\\\\..*\\\\.cruise", ".*\\\\..*\\\\..*\\\\..*\\\\.cruise" },
+    //    DataHost = "*",
+    //    Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable },
+    //    Icon = "@drawable/fscruiser_32dp")]
+
+    //[IntentFilter(new[] { Intent.ActionView },
+    //    DataScheme = "file",
+    //    DataMimeType = "application/vnd.ni.custom",
+    //    // for the DataPathPatterns we need to specify many addition patters because of an unfortunate limitation in androids pattern matching. See https://stackoverflow.com/questions/3400072/pathpattern-to-match-file-extension-does-not-work-if-a-period-exists-elsewhere-i/8599921
+    //    DataPathPatterns = new[] { ".*\\\\.cruise", ".*\\\\..*\\\\.cruise", ".*\\\\..*\\\\..*\\\\.cruise", ".*\\\\..*\\\\..*\\\\..*\\\\.cruise" },
+    //    Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable },
+    //    Icon = "@drawable/fscruiser_32dp")]
+
     public class MainActivity : FormsApplicationActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -22,7 +51,20 @@ namespace FScruiser.Droid
             Xamarin.Toolkit.Effects.Droid.Effects.Init();
             DLToolkit.Forms.Controls.FlowListView.Init();
 
-            var app = new App(new AndroidPlatformInitializer(this));
+            string cruisePath = null;
+
+            //var intent = Intent;
+            //if (intent != null && intent.Action == Intent.ActionView)
+            //{
+            //    var filePath = intent.DataString;
+            //    if(string.IsNullOrEmpty(filePath) && System.IO.File.Exists(filePath))
+            //    {
+            //        cruisePath = filePath;
+            //    }
+
+            //}
+
+            var app = new App(new AndroidPlatformInitializer(this), cruisePath);
 
             LoadApplication(app);
         }
